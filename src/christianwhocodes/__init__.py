cb82faa79b013e3c85aa7af60f14a991c436ff7b
Kevin Wasike Wakhisi <christianwhocodes@gmail.com>
2025-11-19 22:58:16 +0300

    refactor: renamed package name to 'christianwhocodes'

---
+++ src/christianwhocodes/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+
+def main() -> None:
+    print(
+        "...but the people who know their God shall be strong, and carry out great exploits. — Daniel 11:32"
+    )
+
+
+if __name__ == "__main__":
+    main()
--- src/dev/cli.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-
-def main() -> None:
-    print("Developer Tools CLI")
-
-
-if __name__ == "__main__":
-    main()
