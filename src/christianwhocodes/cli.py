aa9d4e6530e6faebec2c0e6fbc54bb6bd4ed14c4
Kevin Wasike Wakhisi <christianwhocodes@gmail.com>
2025-11-19 23:19:46 +0300

    milestone: bump version to first stable release
    - Can publish to testpypi and pypi

--- src/christianwhocodes/cli.py
+++ src/christianwhocodes/cli.py
@@ -1,7 +1,7 @@
 from rich import print
 
 
-def main() -> None:
+def main():
     print(
         "...but the people who know their God shall be strong, and carry out great exploits. [purple]—[/] [bold green]Daniel[/] 11:32"
     )
